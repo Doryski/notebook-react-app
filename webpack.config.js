09a4cfd9556005161e69eabc18e3b3ba5d529d06
doryski <dominatorskyyy@gmail.com>
2020-09-03 16:01:45 +0200

    improved code and performance

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 		path: path.join(__dirname, '/build'),
 		filename: '[name].bundle.js',
 		chunkFilename: '[name].bundle.js',
-		publicPath: 'build/',
+		publicPath: '',
 	},
 	devtool: 'cheap-module-source-map',
 	module: {
