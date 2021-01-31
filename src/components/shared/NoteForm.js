a9cf489e0102da15f67c5b367fd34d5a48a3f047
doryski <dominatorskyyy@gmail.com>
2021-01-31 12:57:36 +0100

    added dynamic maxHeight of textarea

---
+++ src/components/shared/NoteForm.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+	display: inherit;
+	flex-direction: inherit;
+`
+
+export const Label = styled.label`
+	display: flex;
+	flex-direction: column;
+	margin-top: ${({ theme }) => theme.padding.medium};
+	font-weight: 600;
+`
+
+export const Input = styled.input`
+	border: none;
+	border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
+	width: 100%;
+	margin-top: ${({ theme }) => theme.padding.small};
+	background: ${({ theme }) => theme.colors.light};
+	padding: ${({ theme }) =>
+		`${theme.padding.medium} ${theme.padding.small}`};
+	font-family: inherit;
+`
+
+export const Textarea = styled.textarea`
+	display: block;
+	border: none;
+	border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
+	width: 100%;
+	margin-top: ${({ theme }) => theme.padding.small};
+	padding: ${({ theme }) => theme.padding.small};
+	background: ${({ theme }) => theme.colors.light};
+	resize: none;
+	font-family: inherit;
+	height: 100%;
+	line-height: 1.5em;
+	max-height: ${({ maxHeight }) => maxHeight}px;
+
+	::-webkit-scrollbar-track {
+		background: ${({ theme }) => theme.colors.light};
+	}
+	::-webkit-scrollbar-thumb {
+		background: ${({ theme }) => theme.colors.primary};
+	}
+	::-webkit-scrollbar-thumb:hover {
+		background: ${({ theme }) => theme.colors.secondaryLight};
+	}
+`
