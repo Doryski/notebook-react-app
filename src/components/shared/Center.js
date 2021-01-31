8428bb91f0b6025df829b661c3085d621cab6918
doryski <dominatorskyyy@gmail.com>
2021-01-31 12:59:34 +0100

    created component that centers children

---
+++ src/components/shared/Center.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Center = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+`
+export default Center
