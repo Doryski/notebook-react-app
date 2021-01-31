61587e093e67e925beae853b7786cc91d88032e0
doryski <dominatorskyyy@gmail.com>
2021-01-31 13:00:24 +0100

    adjusted component's functionality to modal

--- src/components/AddNotePage.js
+++ src/components/AddNotePage.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import ButtonSection from './ButtonSection'
 import TitleInput from './TitleInput'
 import BodyInput from './BodyInput'
-import { Form } from './StyledComponents/StyledForm'
-import { ADD_PAGE, HOME_PATH } from '../helpers/utils'
+import { Form } from './shared/NoteForm'
+import { ADD_NOTE, HOME_PATH } from '../helpers/utils'
 import useNoteHandler from '../helpers/useNoteHandler'
-import { useHistory } from 'react-router-dom'
+import handleSubmit from '../helpers/handleSubmit'
 
 const AddNotePage = () => {
 	const {
@@ -16,15 +17,19 @@ const AddNotePage = () => {
 		body,
 	} = useNoteHandler()
 	const history = useHistory()
-	const handleSubmit = e => {
-		e.preventDefault()
+	const handleAddBtnClick = () => {
 		handleAddNote()
 		history.push(HOME_PATH)
 	}
 
 	return (
 		<Form onSubmit={handleSubmit}>
-			<ButtonSection page={ADD_PAGE} />
+			<ButtonSection
+				page={ADD_NOTE}
+				title={title}
+				body={body}
+				handleAddBtnClick={handleAddBtnClick}
+			/>
 			<TitleInput
 				title={title}
 				handleTitleChange={handleTitleChange}
